7aa5edad6eed05f2423097238db61ca91cda9ff7
Ikukr-lang <profootballfun@yandex.ru>
2026-03-15 23:25:24 +0300

    Create models.py

---
+++ models.py
@@ -0,0 +1,39 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+from datetime import datetime
+
+db = SQLAlchemy()
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+    tournaments = db.relationship('Tournament', backref='owner', lazy=True)
+
+class Tournament(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    teams = db.relationship('Team', backref='tournament', lazy=True)
+    matches = db.relationship('Match', backref='tournament', lazy=True)
+
+class Team(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    tournament_id = db.Column(db.Integer, db.ForeignKey('tournament.id'))
+    players = db.relationship('Player', backref='team', lazy=True)
+
+class Player(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    team_id = db.Column(db.Integer, db.ForeignKey('team.id'))
+
+class Match(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+    home_team_id = db.Column(db.Integer, db.ForeignKey('team.id'))
+    away_team_id = db.Column(db.Integer, db.ForeignKey('team.id'))
+    home_goals = db.Column(db.Integer, default=0)
+    away_goals = db.Column(db.Integer, default=0)
+    tournament_id = db.Column(db.Integer, db.ForeignKey('tournament.id'))
